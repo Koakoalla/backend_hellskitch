fa54553cb8d51a6dcc516486b373d8955c949ba1
Koakoalla <104795085+Koakoalla@users.noreply.github.com>
2022-11-03 03:35:57 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const { stringify } = require('nodemon/lib/utils');
+
+require('dotenv/config');
+const api = process.env.API_URL;
+
+//middleware
+app.use(express.json());
+app.use(morgan('tiny'));
+
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+
+const Product = mongoose.model('Product', productSchema );
+
+app.get(`$(api)/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'suchara2',
+        image: 'some_url',
+    }
+    res.send(product);
+})
+
+app.post(`${api}/products`, (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    })
+
+    product.save().then((createdProduct=> {
+        res.status(201).json(createdProduct)
+    })).catch((err)=>{
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+})
+
+mongoose.connect(process.env.CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        dbName: 'hellskitchen-database'
+}).then(() =>{
+    console.log('Database connection is ready')
+}).catch((err) => {
+        console.log(err);
+})
+    app.listen(3001, () => {
+    console.log(api);
+    console.log('server is running http://localhost:3001');
+})
