25028a714d6b3690bbb7c1a737bdd5c3548589f1
Koakoalla <iraazazaza@MacBook-Air-Koala.local>
2022-11-06 01:44:48 +0300

    v6

--- routers/products.js
+++ routers/products.js
@@ -5,28 +5,52 @@ const mongoose = require('mongoose');
 const { Category } = require('../models/category');
 
 router.get(`/`, async (req, res) => {
-    const productList = await Product.find();
+    const productList = await Product.find(filter).populate('category');
     if (!productList) {
         res.status(500).json({ success: false });
     }
     res.send(productList);
 });
 
-router.post(`/count`, (req, res) => {
+router.get(`/:id`, async (req, res) => {
+    const product = await Product.findById(req.params.id).populate('category');
+   
+    if (!productList) {
+        res.status(500).json({ success: false });
+    }
+    res.send(product);
+});
+
+router.get(`/:id`, async (req, res) => {
+    const product = await Product.findById(req.params.id).populate('category');
+    if (!product) {
+        res.status(500).json({ success: false });
+    }
+    res.send(product);
+});
+
+router.post(`/`, async (req, res) => {
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+
     const product = new Product({
         name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
+        description: req.body.description,
+        richDescription: req.body.richDescription,
+        image: `${basePath}${fileName}`, // "http://localhost:3000/public/upload/image-2323232"
+        brand: req.body.brand,
+        price: req.body.price,
+        category: req.body.category,
+        countInStock: req.body.countInStock,
+        rating: req.body.rating,
+        numReviews: req.body.numReviews,
+        isFeatured: req.body.isFeatured,
     })
-
-    product.save().then((createdProduct=> {
-        res.status(201).json(createdProduct)
-    })).catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
+    
+    product = await product.save();
+    if(!product)
+    return res.status(500).send('The product cannot be created') 
+    res.send(product);
 })
 // api/
 router.delete('/:id', (req, res) => {
