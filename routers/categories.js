f4a1b80dd7b62328708149b30102fe54d7bb2618
Koakoalla <iraazazaza@MacBook-Air-Koala.local>
2022-11-05 20:21:06 +0300

    v4

--- routers/categories.js
+++ routers/categories.js
@@ -21,4 +21,22 @@ router.post('/', async (req, res) => {
 
     if(!category)
     return res.status(404).send('the category cannot be created!')
+
+    res.send(category);  
 })
+
+
+//avi/v1/asdasdas
+router.delete('/:id', (req, res)=>{
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category) {
+            return res.status(200).json({success: true, message: 'the category is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "category not found!"})
+        }
+    }).catch(err=>{
+       return res.status(500).json({success: false, error: err}) 
+    })
+})
+
+module.exports = router;
