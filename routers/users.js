527cb066844cdc73b287e5fcff0b841897527547
Koakoalla <iraazazaza@MacBook-Air-Koala.local>
2022-11-07 17:42:23 +0300

    v8

---
+++ routers/users.js
@@ -0,0 +1,43 @@
+const {User} = require('../models/user');
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs')
+
+router.get(`/`, async (req, res) => {
+    const userList = await User.find();
+    if (!userList) {
+        res.status(500).json({success: false})
+    }
+    res.send(userList);
+})
+
+router.get('/:id', async(req, res) =>{
+    const user = await User.findById(req.params.id);
+    if(!category) {
+        res.status(500).json({message: 'The User with this ID was already taken' })
+    }
+    res.status(200).send(user);
+})
+
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.icon,
+        passwordHash: bcrypt.hashSync(req.body.password, 10),
+        phone: req.body,phone,
+        isAdmin: req.body.isAdmin,
+        street: req.body.street,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country,
+    })
+    user = await user.save();
+
+    if(!user)
+    return res.status(400).send('the user cannot be created')
+    
+    res.send(user);
+})
+
+module.exports = router;
